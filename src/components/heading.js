832ec21812cd288ced24c4de5b72912c5605cc40
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2021-02-22 15:58:43 +0530

    started the project

---
+++ src/components/heading.js
@@ -0,0 +1,13 @@
+import React from "react";
+import headingCard from "./headingCard";
+function heading() {
+  return (
+    <div>
+      <headingCard />
+      <headingCard />
+      <headingCard />
+    </div>
+  );
+}
+
+export default heading;
---
+++ src/components/headingCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function headingCard() {
+  return <div></div>;
+}
+
+export default headingCard;
