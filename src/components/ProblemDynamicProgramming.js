5fcd624340d770c67c7912bcc174426c79440878
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2021-02-25 22:43:47 +0530

    Added 18 problems of graphs

--- src/components/ProblemDynamicProgramming.js
+++ src/components/ProblemDynamicProgramming.js
@@ -5,114 +5,114 @@ import DynamicProgramming from "../problemComponents/DynamicProgramming";
 var DynamicProgrammingList = [
   {
     id: 1,
-    problemStatement: "L00 : Course Overview ",
+    problemStatement: "Course Overview ",
     problemLink: "No Link",
     tutorialLink: "https://www.youtube.com/watch?v=_dbviFz2aZs",
   },
   {
     id: 2,
-    problemStatement: "L01 : Staircase Problem ",
+    problemStatement: "Staircase Problem ",
     problemLink: "No Link",
     tutorialLink: "https://www.youtube.com/watch?v=2eNnuR0vlFA",
   },
   {
     id: 3,
-    problemStatement: "L02 : Staircase Problem Recursive Approach ",
+    problemStatement: "Staircase Problem Recursive Approach ",
     problemLink:
       "https://practice.geeksforgeeks.org/problems/count-ways-to-reach-the-nth-stair-1587115620/1",
     tutorialLink: "https://www.youtube.com/watch?v=oRBL9som81Q",
   },
   {
     id: 4,
-    problemStatement: "L03 : 2 x N Tiling Problem ",
+    problemStatement: "2 x N Tiling Problem ",
     problemLink:
       "https://practice.geeksforgeeks.org/problems/ways-to-tile-a-floor5836/1",
     tutorialLink: "https://www.youtube.com/watch?v=0j-snD1b-R4",
   },
   {
     id: 5,
-    problemStatement: "L07 : Coin Change",
+    problemStatement: "Coin Change",
     problemLink:
       "https://practice.geeksforgeeks.org/problems/coin-change2448/1",
     tutorialLink: "https://www.youtube.com/watch?v=ximB2-AwoS8",
   },
   {
     id: 6,
-    problemStatement: "L08 : Subset Sum Part 1 ",
+    problemStatement: "Subset Sum Part 1 ",
     problemLink:
       "https://practice.geeksforgeeks.org/problems/subset-sum-problem2014/1",
     tutorialLink: "https://www.youtube.com/watch?v=BT_ACNC47Os",
   },
   {
     id: 7,
-    problemStatement: "L09 : Subset Sum with Linear Space",
+    problemStatement: "Subset Sum with Linear Space",
     problemLink: "No Link",
     tutorialLink: "https://www.youtube.com/watch?v=-httet0UVUU",
   },
   {
     id: 8,
-    problemStatement: "L10 : Printing Result from DP Array",
+    problemStatement: "Printing Result from DP Array",
     problemLink: "No Link",
     tutorialLink: "https://www.youtube.com/watch?v=HjJI1tbtn_M",
   },
   {
     id: 9,
-    problemStatement: "L11 : Min / Max Path Queries On 2D Grid",
+    problemStatement: "Min / Max Path Queries On 2D Grid",
     problemLink: "No Link",
     tutorialLink: "https://www.youtube.com/watch?v=su3cE9RpGJk",
   },
   {
     id: 10,
-    problemStatement: "L12 : Min / Max Path Query on 2D grid Part 2",
+    problemStatement: "Min / Max Path Query on 2D grid Part 2",
     problemLink: "No Link",
     tutorialLink: "https://www.youtube.com/watch?v=Xz-nXdUzEh4",
   },
   {
     id: 11,
-    problemStatement: "E002 : Minimum Falling Path Sum ",
+    problemStatement: "Minimum Falling Path Sum ",
     problemLink: "https://leetcode.com/problems/minimum-falling-path-sum/",
     tutorialLink: "https://www.youtube.com/watch?v=Xr_LRdr9IPQ",
   },
   {
     id: 12,
-    problemStatement: "E003 : Minimum Path Sum",
+    problemStatement: "Minimum Path Sum",
     problemLink: "https://leetcode.com/problems/minimum-path-sum/",
     tutorialLink: "https://www.youtube.com/watch?v=8SgF_mGfRI0",
   },
   {
     id: 13,
-    problemStatement: "L13 : Longest Palindromic Substring ",
+    problemStatement: "Longest Palindromic Substring ",
     problemLink: "No Link",
     tutorialLink: "https://www.youtube.com/watch?v=OjaUemQyDmw",
   },
   {
     id: 14,
-    problemStatement: "E004 : Longest Palindromic Substring ",
+    problemStatement: "Longest Palindromic Substring ",
     problemLink: "https://leetcode.com/problems/longest-palindromic-substring/",
     tutorialLink: "https://www.youtube.com/watch?v=5SrTJ4D9hKw",
   },
   {
     id: 15,
-    problemStatement: "L14 : Unique Ways to reach (N , M) from (1 , 1) ",
+    problemStatement: "Unique Ways to reach (N , M) from (1 , 1) ",
     problemLink:
       "https://practice.geeksforgeeks.org/problems/count-all-possible-paths-from-top-left-to-bottom-right3011/1",
     tutorialLink: "https://www.youtube.com/watch?v=WvfCbcUjBds",
   },
   {
     id: 16,
-    problemStatement: "E005 : Unique Paths ",
+    problemStatement: "Unique Paths ",
     problemLink: "https://leetcode.com/problems/unique-paths/",
     tutorialLink: "https://www.youtube.com/watch?v=Z8mISTcAyjA",
   },
   {
     id: 17,
-    problemStatement: "E006 : Unique Paths II ",
+    problemStatement: "Unique Paths II ",
     problemLink: "https://leetcode.com/problems/unique-paths-ii/",
     tutorialLink: "https://www.youtube.com/watch?v=PeX4IiF9x9w",
   },
   {
     id: 18,
-    problemStatement: "L15 : Rod Cutting Problem ",
+    problemStatement: "Rod Cutting Problem ",
     problemLink:
       "https://practice.geeksforgeeks.org/problems/cutted-segments1642/1",
     tutorialLink: "https://www.youtube.com/watch?v=gl14h24cngc",
@@ -125,21 +125,326 @@ var DynamicProgrammingList = [
   },
   {
     id: 20,
-    problemStatement: "2 Types of knapsack",
+    problemStatement: "Types of knapsack",
     problemLink: "No Link",
     tutorialLink: "https://www.youtube.com/watch?v=l02UxPYRmCQ",
   },
   {
     id: 21,
-    problemStatement: "3 01 Knapsack Recursive",
+    problemStatement: "01 Knapsack Recursive",
     problemLink: "No Link",
     tutorialLink: "https://www.youtube.com/watch?v=kvyShbFVaY8",
   },
   {
     id: 22,
-    problemStatement: "4 01Knapsack Memoization",
+    problemStatement: "01Knapsack Memoization",
     problemLink: "No Link",
     tutorialLink: "https://www.youtube.com/watch?v=fJbIuhs24zQ",
+  },
+  {
+    id: 23,
+    problemStatement:
+      "01 Knapsack Top Down DP(*Video author made mistake in naming Top-Down and Bottom-Up)",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1",
+    tutorialLink: "https://www.youtube.com/watch?v=ntCGbPMeqgg",
+  },
+  {
+    id: 24,
+    problemStatement: "Identification of Knapsack Problems and Introduction",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=iBnWHZmlIyY",
+  },
+  {
+    id: 25,
+    problemStatement: "Subset Sum Problem",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/subset-sum-problem2014/1",
+    tutorialLink: "https://www.youtube.com/watch?v=_gPcYovP7wc",
+  },
+  {
+    id: 27,
+    problemStatement: "Equal Sum Partition Problem",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=UmMh7xp07kY",
+  },
+  {
+    id: 28,
+    problemStatement: "Count of Subsets Sum with a Given Sum",
+    problemLink:
+      "https://www.geeksforgeeks.org/count-of-subsets-with-sum-equal-to-x/",
+    tutorialLink: "https://www.youtube.com/watch?v=F7wqWbqYn9g",
+  },
+  {
+    id: 29,
+    problemStatement: "Minimum Subset Sum Difference",
+    problemLink:
+      "https://www.geeksforgeeks.org/partition-a-set-into-two-subsets-such-that-the-difference-of-subset-sums-is-minimum/",
+    tutorialLink: "https://www.youtube.com/watch?v=-GtpxG6l_Mc",
+  },
+  {
+    id: 30,
+    problemStatement: "Count the number of subset with a given difference",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=ot_XBHyqpFc",
+  },
+  {
+    id: 31,
+    problemStatement: "Target sum",
+    problemLink: "https://leetcode.com/problems/target-sum/",
+    tutorialLink: "https://www.youtube.com/watch?v=Hw6Ygp3JBYw",
+  },
+  {
+    id: 32,
+    problemStatement: "Unbounded Knapsack",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/knapsack-with-duplicate-items4201/1",
+    tutorialLink: "https://www.youtube.com/watch?v=aycn9KO8_Ls",
+  },
+  {
+    id: 33,
+    problemStatement: "Rod Cutting Problem",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/cutted-segments1642/1",
+    tutorialLink: "https://www.youtube.com/watch?v=SZqAQLjDsag",
+  },
+  {
+    id: 34,
+    problemStatement: "Coin change problem: Maximum number of ways",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/coin-change2448/1",
+    tutorialLink: "https://www.youtube.com/watch?v=I4UR2T6Ro3w",
+  },
+  {
+    id: 35,
+    problemStatement: "Coin change problem: Maximum number of ways",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/coin-change2448/1",
+    tutorialLink: "https://www.youtube.com/watch?v=I4UR2T6Ro3w",
+  },
+  {
+    id: 36,
+    problemStatement: "Coin change problem: Minimum number of coins",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=I-l6PBeERuc",
+  },
+  {
+    id: 36,
+    problemStatement: "Coin change problem Contd.",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/number-of-coins1824/1",
+    tutorialLink: "https://www.youtube.com/watch?v=rMfOgY07TFs",
+  },
+  {
+    id: 37,
+    problemStatement: "Longest common subsequence Introduction",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=4dMlCZTONj8",
+  },
+  {
+    id: 38,
+    problemStatement: "Longest common subsequence Recursive",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=4Urd0a0BNng",
+  },
+  {
+    id: 39,
+    problemStatement: "Longest common subsequence Memoization",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=g_hIx4yn9zg",
+  },
+  {
+    id: 40,
+    problemStatement: "Longest common subsequence Top down DP",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/longest-common-subsequence-1587115620/1",
+    tutorialLink: "https://www.youtube.com/watch?v=hR3s9rGlMTU",
+  },
+  {
+    id: 41,
+    problemStatement: "Longest Common Substring",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/longest-common-substring1452/1",
+    tutorialLink: "https://www.youtube.com/watch?v=HrybPYpOvz0",
+  },
+  {
+    id: 42,
+    problemStatement: "Printing Longest common subsequence",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=x5hQvnUcjiM",
+  },
+  {
+    id: 43,
+    problemStatement: "Shortest Common SuperSequence",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/shortest-common-supersequence0322/1",
+    tutorialLink: "https://www.youtube.com/watch?v=823Grn4_dCQ",
+  },
+  {
+    id: 44,
+    problemStatement:
+      "Minimum Number of Insertion and Deletion to convert String a to String b",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/minimum-number-of-deletions-and-insertions0209/1",
+    tutorialLink: "https://www.youtube.com/watch?v=-fx6aDxcWyg",
+  },
+  {
+    id: 45,
+    problemStatement: "Longest Palindromic Subsequence",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=wuOOOATz_IA",
+  },
+  {
+    id: 46,
+    problemStatement:
+      "Minimum number of deletion in a string to make it a palindrome",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/minimum-deletitions/0",
+    tutorialLink: "https://www.youtube.com/watch?v=CFwCCNbRuLY",
+  },
+  {
+    id: 47,
+    problemStatement: "Print shortest common Supersequence",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=VDhRg-ZJTuc",
+  },
+  {
+    id: 48,
+    problemStatement: "Longest repeating subsequence",
+    problemLink: "https://www.geeksforgeeks.org/longest-repeating-subsequence/",
+    tutorialLink: "https://www.youtube.com/watch?v=hbTaCmQGqLg",
+  },
+  {
+    id: 49,
+    problemStatement: "Sequence Pattern Matching",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/wildcard-pattern-matching/1",
+    tutorialLink: "https://www.youtube.com/watch?v=QVntmksK2es",
+  },
+  {
+    id: 50,
+    problemStatement:
+      "Minimum number of insertion in a string to make it a palindrome",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/form-a-palindrome/0",
+    tutorialLink: "https://www.youtube.com/watch?v=AEcRW4ylm_c",
+  },
+  {
+    id: 51,
+    problemStatement:
+      "Matrix chain multiplication Introduction Identification and General Format",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=D7AFvtnDeMU",
+  },
+  {
+    id: 52,
+    problemStatement: "Matrix Chain Multiplication Recursive",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=kMK148J9qEE",
+  },
+  {
+    id: 53,
+    problemStatement: "Matrix chain multiplication Memoization",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/matrix-chain-multiplication0303/1",
+    tutorialLink: "https://www.youtube.com/watch?v=9uUVFNOT3_Y",
+  },
+  {
+    id: 54,
+    problemStatement: "Palindrome Partitioning Recursive",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=szKVpQtBHh8",
+  },
+  {
+    id: 55,
+    problemStatement: "Palindrome Partitioning Memoization",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=fOUlNlawdAU",
+  },
+  {
+    id: 56,
+    problemStatement: "Palindrome Partitioning Memoized Optimisation",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/palindromic-patitioning4845/1",
+    tutorialLink: "https://www.youtube.com/watch?v=9h10fqkI7Nk",
+  },
+  {
+    id: 57,
+    problemStatement:
+      "Evaluate Expression to True Boolean Parenthesization Recursive",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=pGVguAcWX4g",
+  },
+  {
+    id: 58,
+    problemStatement:
+      "Evaluate Expression To True Boolean Parenthesization Memoized",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/boolean-parenthesization5610/1",
+    tutorialLink: "https://www.youtube.com/watch?v=bzXM1Zond9U",
+  },
+  {
+    id: 59,
+    problemStatement: "Scrambled String Recursive",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=SqA0o-DGmEw",
+  },
+  {
+    id: 60,
+    problemStatement: "crambled String Memoized",
+    problemLink: "https://www.interviewbit.com/problems/scramble-string/#",
+    tutorialLink: "https://www.youtube.com/watch?v=VyHEglhbm-A",
+  },
+  {
+    id: 61,
+    problemStatement: "Egg Dropping Problem Recursive",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=S49zeUjeUL0",
+  },
+  {
+    id: 62,
+    problemStatement: "Egg Dropping Problem Memoization",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=gr2NtY-2QUY",
+  },
+  {
+    id: 63,
+    problemStatement: "Egg Dropping Problem Memoization Optimization",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/egg-dropping-puzzle-1587115620/1",
+    tutorialLink: "https://www.youtube.com/watch?v=jkygQmOiCCI",
+  },
+  {
+    id: 64,
+    problemStatement:
+      "Dynamic programming on Trees Introduction and Identification",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=qZ5zayHSH2g",
+  },
+  {
+    id: 65,
+    problemStatement: "Dynamic Programming on Tree General Syntax",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=d1u2t018Kjg",
+  },
+  {
+    id: 66,
+    problemStatement: "Diameter of a Binary Tree",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/diameter-of-binary-tree/1",
+    tutorialLink: "https://www.youtube.com/watch?v=zmPj_Ee3B8c",
+  },
+  {
+    id: 67,
+    problemStatement: "Maximum Path Sum | From any node to any node",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=Osz-Vwer6rw",
+  },
+  {
+    id: 68,
+    problemStatement: "Maximum Path sum | From leaf node to leaf node",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=ArNyupe-XH0",
   },
 ];
 
--- src/components/ProblemGraphs.js
+++ src/components/ProblemGraphs.js
@@ -5,15 +5,121 @@ import Graphs from "../problemComponents/Graphs";
 var GraphsList = [
   {
     id: 1,
-    problemStatement: "pattern1",
-    problemLink: "problem1",
-    tutorialLink: 0,
+    problemStatement: "Types of graph",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=caAVlibiTkY",
   },
   {
     id: 2,
-    problemStatement: 1,
-    problemLink: 1,
-    tutorialLink: 1,
+    problemStatement:
+      "Representation of graph using adjacency matrix and adjacency list",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=4R7chuhzq7k",
+  },
+  {
+    id: 3,
+    problemStatement: "Depth first search | DFS | Depth first traversal",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1",
+    tutorialLink: "https://www.youtube.com/watch?v=CvUMf8c2JFo",
+  },
+  {
+    id: 4,
+    problemStatement: "Breadth first search | BFS | Breadth first traversal",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/bfs-traversal-of-graph/1",
+    tutorialLink: "https://www.youtube.com/watch?v=YYq38LTz774",
+  },
+  {
+    id: 5,
+    problemStatement: "Detect cycle in a directed graph",
+    problemLink: "https://www.geeksforgeeks.org/detect-cycle-in-a-graph/",
+    tutorialLink: "https://www.youtube.com/watch?v=0dJmTuMrUZM",
+  },
+  {
+    id: 6,
+    problemStatement:
+      "Detect cycle in an undirected graph | Graph coloring method",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1",
+    tutorialLink: "https://www.youtube.com/watch?v=L0DcePeWHnM",
+  },
+  {
+    id: 7,
+    problemStatement: "Number of islands ",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/find-the-number-of-islands/1",
+    tutorialLink: "https://www.youtube.com/watch?v=__98uL6wst8",
+  },
+  {
+    id: 8,
+    problemStatement: "Possible Bipartition | Bipartite graph | Graph coloring",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/bipartite-graph/1",
+    tutorialLink: "https://www.youtube.com/watch?v=0ACfAqs8mm0",
+  },
+  {
+    id: 9,
+    problemStatement: "Course Schedule | Deadlock detection | Graph coloring",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=kXy0ABd1vwo",
+  },
+  {
+    id: 10,
+    problemStatement: "Cheapest Flights Within K Stops | DFS + Pruning ",
+    problemLink:
+      "https://leetcode.com/problems/cheapest-flights-within-k-stops/",
+    tutorialLink: "https://www.youtube.com/watch?v=60RbWlDFsmI",
+  },
+  {
+    id: 11,
+    problemStatement: "Reconstruct Itinerary",
+    problemLink: "https://leetcode.com/problems/reconstruct-itinerary/",
+    tutorialLink: "https://www.youtube.com/watch?v=WYqsg5dziaQ",
+  },
+  {
+    id: 12,
+    problemStatement: "Clone graph ",
+    problemLink: "https://leetcode.com/problems/clone-graph/",
+    tutorialLink: "https://www.youtube.com/watch?v=f2EfGComRKM",
+  },
+  {
+    id: 13,
+    problemStatement: "Topological sort | Course schedule 2",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=qe_pQCh09yU",
+  },
+  {
+    id: 14,
+    problemStatement: "Kahn's algorithm | Topological sort | Course schedule 2",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/topological-sort/1",
+    tutorialLink: "https://www.youtube.com/watch?v=tggiFvaxjrY",
+  },
+  {
+    id: 15,
+    problemStatement: "Disjoint Set | UNION and FIND",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=eTaWFhPXPz4",
+  },
+  {
+    id: 16,
+    problemStatement: "Disjoint set UNION by RANK and Path Compression",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=kaBX2s3pYO4",
+  },
+  {
+    id: 17,
+    problemStatement: "Spanning Tree | MST | Graph Theory",
+    problemLink: "No Link",
+    tutorialLink: "https://www.youtube.com/watch?v=jMioOe2eTcY",
+  },
+  {
+    id: 18,
+    problemStatement: "Prims algorithm | MST | Code implementation",
+    problemLink:
+      "https://practice.geeksforgeeks.org/problems/minimum-spanning-tree/1",
+    tutorialLink: "https://www.youtube.com/watch?v=xthRL0lcx2w",
   },
 ];
 
